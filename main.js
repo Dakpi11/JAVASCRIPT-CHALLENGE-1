fa816b43b74af5bbd0cac309b14292bd161f615d
dakpi11 <dakpi11@gmail.com>
2023-07-25 20:11:23 -0400

    question 3 done

--- main.js
+++ main.js
@@ -115,6 +115,16 @@ to the console*/
 
 const isIsland = "false";
 
+/* Question 3
+Set the value of 'language' to the language spoken where you live (some
+    countries have multiple languages, but just choose one)
+    Think about which variables should be const variables (which values will never
+    change, and which might change?). Then, change these variables to const.
+    Try to change one of the changed variables now, and observe what happens.*/
+    
+    const language = "English"
 
-
-
+    console.log(isIsland);
+    console.log(language);
+    
+    
