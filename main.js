b9ab3eab66ed27327ffa5f63d050a313d88365fb
dakpi11 <dakpi11@gmail.com>
2023-08-13 12:29:25 -0400

    (Question6)

--- main.js
+++ main.js
@@ -128,6 +128,15 @@ Log the types of 'isIsland', 'population', 'country' and 'language'
 to the console*/
 
 const isIsland = false
+var language; 
+
+console.log(typeof(isIsland));
+
+console.log(typeof(population));
+
+console.log(typeof(country));
+
+console.log(typeof(language));
 
 
 /* Question 3
@@ -136,11 +145,9 @@ Set the value of 'language' to the language spoken where you live (some
     Think about which variables should be const variables (which values will never
     change, and which might change?). Then, change these variables to const.
     Try to change one of the changed variables now, and observe what happens.*/
-    
-    const language = "English"
+ 
+  
 
-    console.log(isIsland);
-    console.log(language);
     
     /* Question 4 
     Based on the variables you created, create a new variable 'description'
